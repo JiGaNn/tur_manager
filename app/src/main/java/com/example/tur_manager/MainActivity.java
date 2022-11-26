5e3623960092c165a6c985ed63b65037d9d38c7d
Jigann <evgenij29082002@gmail.com>
2022-11-26 17:10:23 +0800

    Refactoring

--- app/src/main/java/com/example/tur_manager/MainActivity.java
+++ app/src/main/java/com/example/tur_manager/MainActivity.java
@@ -6,7 +6,6 @@ import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.ArraySet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,26 +13,18 @@ import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private EditText user_field;
@@ -42,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private MultiAutoCompleteTextView res;
 
     String[] cities = new String[50];
+    HashMap<String, String> iataMap = new HashMap<>();
 
     ProgressDialog progressDialog;
     @Override
@@ -58,51 +50,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 res.setText("");
-                HashMap<String, String> iataMap = new HashMap<>();
 
                 if(user_field.getText().toString().trim().equals(""))
                     Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
 
                 String input = user_field.getText().toString();
                 cities = input.split(" ");
-                String origin = "", destination = "";
-                for (int i=0; i < cities.length-1; i++) {
-                    origin = cities[i];
-                    destination = cities[i+1];
-                    String iataUrl = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20"+destination;
-                    IataCode iata = new IataCode();
-                    iata.execute(iataUrl);
-                    try {
-                        JSONObject jsonObject = iata.get();
-                        iataMap.put(origin, jsonObject.getJSONObject("origin").getString("iata"));
-                        iataMap.put(destination, jsonObject.getJSONObject("destination").getString("iata"));
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                textView.setText(iataMap.toString());
+
                 try {
-                    getContent(iataMap);
+                    getIata();
+                    textView.setText(iataMap.toString());
+                    getContent();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
+    public void getIata() throws ExecutionException, InterruptedException, JSONException {
+        String origin = "", destination = "";
+        for (int i=0; i < cities.length-1; i++) {
+            origin = cities[i];
+            destination = cities[i+1];
+            String iataUrl = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20"+destination;
+            IataCode iata = new IataCode();
+            iata.execute(iataUrl);
+            JSONObject jsonObject = iata.get();
+            iataMap.put(origin, jsonObject.getJSONObject("origin").getString("iata"));
+            iataMap.put(destination, jsonObject.getJSONObject("destination").getString("iata"));
+        }
+    }
     // алгоритм нахождения оптимального тура
-    public void getContent(HashMap<String, String> imap) throws ExecutionException, InterruptedException {
-        String url, minKey = "", destination, origin = imap.get(cities[0]);
+    public void getContent() throws ExecutionException, InterruptedException {
+        String url, minKey = "", destination, origin = iataMap.get(cities[0]);
         double value, minValue=1000000;
-        imap.remove(cities[0]);
-        while (!imap.isEmpty()) { // пока карта не пуста
-            for (String key : imap.keySet()) { // обрабатываем каждый ключ карты
-                destination = imap.get(key); // город прибытия
+        iataMap.remove(cities[0]);
+        while (!iataMap.isEmpty()) { // пока карта не пуста
+            for (String key : iataMap.keySet()) { // обрабатываем каждый ключ карты
+                destination = iataMap.get(key); // город прибытия
                 url = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=" + origin + "&destination=" + destination + "&direct=true&limit=10&token=514ed26973e8ac0aa6fb96f3e3de891d";
                 Content content = new Content();
                 content.execute(url);
@@ -113,8 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             res.append(origin + " - " + minKey + ": " + minValue + "\n");
-            origin = imap.get(minKey); // будем вылетать из того города, куда прибыли
-            imap.remove(minKey); // удаляем ключ из карты
+            origin = iataMap.get(minKey); // будем вылетать из того города, куда прибыли
+            iataMap.remove(minKey); // удаляем ключ из карты
         }
     }
     private class Content extends AsyncTask<String, String, Double>{
